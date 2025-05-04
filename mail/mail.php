e70816c92eb5a5789c94f9016cd64cbb42f86625
Subodh Kumar Gupta <subugupta08@gmail.com>
2025-05-04 18:11:52 +0530

    Update mail.php

--- mail/mail.php
+++ mail/mail.php
@@ -1,20 +1,20 @@
-<?php
-$name = $_POST{'name'};
-$email = $_POST{'email'};
-$phone = $_POST{'phone'};
-$subject = $_POST{'subject'};
-$message = $_POST['message'];
+// <?php
+// $name = $_POST{'name'};
+// $email = $_POST{'email'};
+// $phone = $_POST{'phone'};
+// $subject = $_POST{'subject'};
+// $message = $_POST['message'];
 
-$email_message = "
+// $email_message = "
 
-Name: ".$name."
-Email: ".$email."
-Phone: ".$phone."
-Subject: ".$subject."
-Message: ".$message."
+// Name: ".$name."
+// Email: ".$email."
+// Phone: ".$phone."
+// Subject: ".$subject."
+// Message: ".$message."
 
-";
+// ";
 
-mail ("connect@kdnavigators.com" , "New Message", $email_message);
-header("location: ../mail-success.html");
-?>
+// mail ("connect@kdnavigators.com" , "New Message", $email_message);
+// header("location: ../mail-success.html");
+// ?>
